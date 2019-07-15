05a3c8dd57cb4ac9bd96da96ff5bc17d93f95496
Martin-4Spaces <faaax@msn.com>
2019-07-15 09:59:40 +0200

    Avoid CI4 Debugtool

--- Hooks/PreController.php
+++ Hooks/PreController.php
@@ -21,40 +21,31 @@ class PreController {
 
             $routes->get('openidconfiguration', function() use ($response) {
                 \AuthExtension\OAuth2\OpenIdConfiguration::handle($response);
-                exit(0);
             });
             $routes->get('authorize', function() use ($response) {
                 \AuthExtension\OAuth2\Authorize::handle($response);
-                exit(0);
             });
             $routes->get('userinfo', function() use ($response) {
                 \AuthExtension\OAuth2\UserInfo::handle($response);
-                exit(0);
             });
             $routes->post('token', function() use ($response) {
                 \AuthExtension\OAuth2\Token::handle($response);
-                exit(0);
             });
             $routes->post('revocation', function() use ($response) {
                 \AuthExtension\OAuth2\Revocation::handle($response);
-                exit(0);
             });
             $routes->get('endsession', function() use ($response) {
                 \AuthExtension\OAuth2\EndSession::handle(Services::request(), $response);
-                exit(0);
             });
             $routes->get('checksession', function() use ($response) {
                 \AuthExtension\OAuth2\CheckSession::handle();
-                exit(0);
             });
             $routes->get('check', function() use ($response) {
                 \AuthExtension\OAuth2\Check::handle($response);
-                exit(0);
             });
             $routes->cli('check/(:segment)', function($accessToken) use ($response) {
                 if($accessToken) $_GET['access_token'] = $accessToken;
                 \AuthExtension\OAuth2\Check::handle($response);
-                exit(0);
             });
         }
     }
