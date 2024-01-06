37ef77f6b8696b57d8dae6afde60c6bae4463ab3
martin <faaax@msn.com>
2024-01-06 09:15:18 +0100

    Login with username only (for SSO purposes)

--- AuthExtension.php
+++ AuthExtension.php
@@ -7,7 +7,7 @@ use AuthExtension\OAuth2\ServerLib;
 
 class AuthExtension {
 
-    public static function checkLogin(string $username, string $password, string $scope = null): string {
+    public static function checkLoginWithUsernamePassword(string $username, string $password, string $scope = null): string {
         // Check if everything is good
         /** @var User $user */
         $user = (new UserModel())
@@ -32,7 +32,45 @@ class AuthExtension {
     }
 
     public static function login(string $username, string $password, string $scope = null): string {
-        $loginResponse = AuthExtension::checkLogin($username, $password, $scope);
+        $loginResponse = AuthExtension::checkLoginWithUsernamePassword($username, $password, $scope);
+
+        switch ($loginResponse) {
+            case LoginResponse::Success:
+            case LoginResponse::RenewPassword:
+
+                $user = (new UserModel())
+                    ->where('username', $username)
+                    ->find();
+
+                $session = session();
+                $session->set('user_id', $user->id);
+        }
+
+        return $loginResponse;
+    }
+
+    public static function checkLoginWithUsername(string $username, string $password, string $scope = null): string {
+        // Check if everything is good
+        /** @var User $user */
+        $user = (new UserModel())
+            ->where('username', $username)
+            ->find();
+        if (!$user->exists()) {
+            // Username not found
+            return LoginResponse::UnknownUser;
+        }
+
+        if ($scope && isset($user->scope)) {
+            if (count(array_diff(explode(' ', $scope), explode(' ', $user->scope))) > 0) {
+                return LoginResponse::WrongScope;
+            }
+        }
+
+        return $user->renew_password ? LoginResponse::RenewPassword : LoginResponse::Success;
+    }
+
+    public static function loginWithUsername(string $username, string $scope = null): string {
+        $loginResponse = AuthExtension::checkLoginWithUsername($username, $scope);
 
         switch ($loginResponse) {
             case LoginResponse::Success:
